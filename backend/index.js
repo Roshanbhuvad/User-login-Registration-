7dd527c8b21b0b11c64a117b0ace5dce72b8aa14
Roshan <roshanbhuvad15@gmail.com>
2020-09-26 19:00:05 +0530

    header, login & register pages added

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,18 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(cors());
-
+const errorHandlers = require("./handlers/errorHandlers");
+app.use(errorHandlers.notFound);
+app.use(errorHandlers.mongooseErrors);
+if (process.env.ENV === "DEVELOPMENT") {
+    app.use(errorHandlers.developmentErrors);
+} else {
+    app.use(errorHandlers.productionErrors);
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
