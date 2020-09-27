3a77fbd598ea732a12e389db4c0cca21ded87670
Roshan <roshanbhuvad15@gmail.com>
2020-09-27 16:03:46 +0530

    deploying on heroku

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
+const {MONGODB_CONNECTION_STRING, JWT_SECRET} = require("./config/keys.js")
 require("dotenv").config();
 
 // set up express
@@ -8,14 +10,20 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("front/build"))
+  const path = require("path")
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "front", "build", "index.html"))
+  })
+} 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
 
 
 // set up mongooses
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+mongoose.connect(MONGODB_CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-
+const {JWT_SECRET} = require("../config/keys.js")
 const auth = (req, res, next) => {
     try {
         const token = req.header("x-auth-token");
@@ -7,7 +7,7 @@ const auth = (req, res, next) => {
             return res.status(401).json({
                 msg: "No authentication token, authorization denied."
             });
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        const verified = jwt.verify(token,JWT_SECRET);
         if (!verified)
             return res.status(401).json({
                 msg: "Token verification failed, authorization denied."
